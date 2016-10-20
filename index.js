214582315a9416c954741083ff1bb6f9d6950737
Mihail Slavchev <mihail.slavchev@telerik.com>
2016-10-20 15:57:34 +0300

    exports test functions

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-if (global.NSObject) {
+exports.runAllTest = function() {
+    runImportTests();
+    runRequireTests();
+    runWeakRefTests();
+    runRuntimeTests();
+    runWorkerTests();
+}
+
+exports.runImportTests = function() {
     require("./Import");
 }
 
-require("./Require");
-require("./WeakRef");
-require("./RuntimeTests");
-require("./Workers");
+exports.runRequireTests = function() {
+    require("./Require");
+}
+
+exports.runWeakRefTests = function() {
+    require("./WeakRef");
+}
+
+exports.runRuntimeTests = function() {
+    require("./RuntimeTests");
+}
+
+exports.runWorkerTests = function() {
+    require("./Workers");
+}
